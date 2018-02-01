0aea1f75c4867fc7af6f9961f629876a20135a2e
Maxime <boczulak.maxime@hotmail.fr>
2018-02-01 13:31:35 +0100

    project updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-const gulp = require('gulp'),
-      sass = require('gulp-sass'),
-      del  = require('del');
+const gulp   = require('gulp'),
+      csso   = require('gulp-csso'),
+      sass   = require('gulp-sass'),
+			del    = require('del'),
+			minify = require('gulp-minify');
 
 // Delete all CSS files
 gulp.task('css:clean', function() {
@@ -11,6 +13,7 @@ gulp.task('css:clean', function() {
 gulp.task('css:compile', ['css:clean'], function() {
 	return gulp.src('src/scss/*.scss')
 			.pipe(sass())
+			.pipe(csso())
 			.pipe(gulp.dest('dist/css'));
 });
 
@@ -20,9 +23,21 @@ gulp.task('html:clean', function() {
 });
 
 // Copy all HTML files
-gulp.task('html:copy', function() {
+gulp.task('html:copy', ['html:clean'], function() {
 	return gulp.src('src/**/*.html')
 		.pipe(gulp.dest('dist/'));
+});
+
+// Delete all JavaScript files
+gulp.task('js:clean', function() {
+	return del('dist/js/*.js', { force: true });
+});
+
+// Compile all JavaScript files
+gulp.task('js:compile', ['js:clean'], function() {
+	return gulp.src('src/js/*.js')
+		.pipe(minify())
+		.pipe(gulp.dest('dist/js'));
 });
 
 // Delete all static files such as images etc.
@@ -35,11 +50,11 @@ gulp.task('static:clean', function() {
 });
 
 gulp.task('static:copy', ['static:clean'], function() {
-	return gulp.src('src/img/**/*')
-			.pipe(gulp.dest('dist/img/'));
+	return gulp.src('src/static/**/*')
+			.pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['css:compile', 'html:copy', 'static:copy']);
+gulp.task('build', ['static:clean','css:compile', 'html:copy', 'js:compile', 'static:copy']);
 
 gulp.task('develop', ['build'], function() {
 	gulp.watch('src/scss/*', ['css:compile']); // watch for changes in SCSS
